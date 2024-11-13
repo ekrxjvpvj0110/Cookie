f7faedf40ff3e1e9143ae9ca66566604fe828f39
ekrxjvpvj0110 <ekrxjvpvj0110@gmail.com>
2024-11-13 20:21:25 +0900

    feat: ui 위치 정보를 담은 스크립터블 오브젝트 추가
    해당 so를 이용하여 코인이나 도넛의 이동 위치를 설정해줌

---
+++ Assets/Scripts/ScriptableObject/UIPositionSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "UIPositionSO_", menuName = "DonutSO/UIPositionSO", order = 0)]
+public class UIPositionSO : ScriptableObject
+{
+    public Vector3 positon;
+}
